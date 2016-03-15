530ad78fab7bef56bd03bd60e46572c86d86e72f
Xing <813106093@qq.com>
2016-03-15 20:24:12 +0800

    'add'

--- gulpfile.js
+++ gulpfile.js
@@ -12,76 +12,9 @@ var paths = {
 	"assets": "./assets",
     "build": "./build"
 }
-
+//------------------------开发模式1--------------------------------------//
 // The development server (the recommended option for development)
 gulp.task("default", ["webpack-dev-server"]);
-
-//for development
-gulp.task('dev', ['pack:build-dev'], function(){
-    run('removeScript');
-    //监听src目录 如果更改了文件则重新打包
-    gulp.watch(["src/**/*"], ["pack:build-dev"]);
-});
-
-//for production
-gulp.task('release', ['pack:build-release'], function(){
-    run('minhtmlAndRemoveScript');
-});
-
-// clean assets
-gulp.task('clean:dev', function() {
-    var clean = require('gulp-clean');
-    return gulp.src(paths.build, {read: true}).pipe(clean());
-});
-// clean build
-gulp.task('clean:release', function() {
-    var clean = require('gulp-clean');
-
-    return gulp.src(paths.assets, {read: true}).pipe(clean());
-});
-// run webpack pack
-gulp.task('pack:build-release', ['clean:release'], function(callback) {
-    webpack(webpackConf, function(err, stats) {
-        if(err) throw new gutil.PluginError('webpack', err);
-        gutil.log('[webpack]', stats.toString({colors: true}));
-        callback();
-    });
-});
-
-// run webpack pack
-gulp.task('pack:build-dev', ['clean:dev'], function(callback) {
-    webpack(webpackConf, function(err, stats) {
-        if(err) throw new gutil.PluginError('webpack', err);
-        gutil.log('[webpack]', stats.toString({colors: true}));
-        callback();
-    });
-});
-
-gulp.task('removeScript', function() {
-    var replace = require('gulp-replace');
-    // var htmlmin = require('gulp-htmlmin');
-
-    return gulp
-        .src(paths.build + '/**/*.html')
-        .pipe(replace(/<script(.+)?data-debug([^>]+)?><\/script>/g, ''))
-        .pipe(gulp.dest(paths.build));
-});
-
-gulp.task('minhtmlAndRemoveScript', function() {
-    var replace = require('gulp-replace');
-    var htmlmin = require('gulp-htmlmin');
-
-    return gulp
-        .src(paths.assets + '/**/*.html')
-        .pipe(replace(/<script(.+)?data-debug([^>]+)?><\/script>/g, ''))
-        // @see https://github.com/kangax/html-minifier
-        .pipe(htmlmin({
-            collapseWhitespace: true,
-            removeComments: true
-        }))
-        .pipe(gulp.dest(paths.assets));
-});
-
 
 gulp.task("webpack-dev-server", function(callback) {
     // Start a webpack-dev-server
@@ -108,3 +41,85 @@ gulp.task("webpack-dev-server", function(callback) {
         // callback();
     });
 });
+
+//-----------------------------开发模式2---------------------------------//
+//for development
+gulp.task('dev', ['pack:build-dev'], function(){
+    run('removeScript');
+    //监听src目录 如果更改了文件则重新打包
+    gulp.watch(["src/**/*"], ["pack:build-dev"]);
+});
+
+// run webpack pack
+gulp.task('pack:build-dev', ['clean:dev'], function(callback) {
+    webpack(webpackConf, function(err, stats) {
+        if(err) throw new gutil.PluginError('webpack', err);
+        gutil.log('[webpack]', stats.toString({colors: true}));
+        callback();
+    });
+});
+
+gulp.task('removeScript', function() {
+    var replace = require('gulp-replace');
+    // var htmlmin = require('gulp-htmlmin');
+
+    return gulp
+        .src(paths.build + '/**/*.html')
+        .pipe(replace(/<script(.+)?data-debug([^>]+)?><\/script>/g, ''))
+        .pipe(gulp.dest(paths.build));
+});
+
+// clean assets
+gulp.task('clean:dev', function() {
+    var clean = require('gulp-clean');
+    return gulp.src(paths.build, {read: true}).pipe(clean());
+});
+
+//---------------------------生产模式-----------------------------------//
+
+//for production
+gulp.task('release', ['pack:build-release'], function(){
+    run('minhtmlAndRemoveScript');
+});
+
+// run webpack pack
+gulp.task('pack:build-release', ['clean:release'], function(callback) {
+    webpack(webpackConf, function(err, stats) {
+        if(err) throw new gutil.PluginError('webpack', err);
+        gutil.log('[webpack]', stats.toString({colors: true}));
+        callback();
+    });
+});
+
+// clean build
+gulp.task('clean:release', function() {
+    var clean = require('gulp-clean');
+
+    return gulp.src(paths.assets, {read: true}).pipe(clean());
+});
+
+gulp.task('minhtmlAndRemoveScript', function() {
+    var replace = require('gulp-replace');
+    var htmlmin = require('gulp-htmlmin');
+
+    return gulp
+        .src(paths.assets + '/**/*.html')
+        .pipe(replace(/<script(.+)?data-debug([^>]+)?><\/script>/g, ''))
+        // @see https://github.com/kangax/html-minifier
+        .pipe(htmlmin({
+            collapseWhitespace: true,
+            removeComments: true
+        }))
+        .pipe(gulp.dest(paths.assets));
+});
+
+
+
+
+
+
+
+
+
+
+
